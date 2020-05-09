1a4a384b8c5a4fce5630b569ffaefaf518510555
Thomas Stang <tstang@popsugar.com>
2020-05-09 14:39:52 -0700

    More project setup

--- api/Firebase.js
+++ api/Firebase.js
@@ -3,18 +3,19 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const config = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DB_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DB_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Protect against multiple instances of firebase
 if (!firebase.apps.length) {
-    firebase.initializeApp(config);
+  firebase.initializeApp(config);
 }
 
 /**
@@ -24,13 +25,12 @@ if (!firebase.apps.length) {
  * @namespace Firebase
  */
 class Firebase {
-    constructor() {
-        this.firebase = firebase;
-        this.firestore = firebase.firestore;
-        this.db = this.firestore();
-        this.listCollectionPath = 'lists';
-        this.listItemCollectionPath = 'listItems';
-    }
+  constructor() {
+    this.firebase = firebase;
+    this.firestore = firebase.firestore;
+    this.db = this.firestore();
+    this.gameCollectionPath = 'games';
+  }
 }
 
 export default Firebase;
